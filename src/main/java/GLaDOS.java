18bcb2be4b0d6019f7250c4c863a75d827947caa
EnderSky <jothamlimjl@hotmail.com>
2026-01-23 14:38:51 +0800

    feat: added list command

--- src/main/java/GLaDOS.java
+++ src/main/java/GLaDOS.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class GLaDOS {
     public static void intro() {
         String logo =   "  ____ _          ____   ___  ____  \r\n" + //
@@ -9,11 +11,13 @@ public class GLaDOS {
         System.out.println(logo);
         System.out.println("Hello, and again, welcome to the Aperture Science Computer-Aided Enrichment Center.");
         System.out.println("What can I do for you today?");
-        System.out.println("___________________________________________________________________________________\n"); 
+        GLaDOS.printLine();
     }
 
     public static void listen() {
         String exitCommand = "bye";
+        String listCommand = "list";
+        ArrayList<String> storedList = new ArrayList<>();
 
         java.util.Scanner scanner = new java.util.Scanner(System.in);
         String input;
@@ -22,23 +26,41 @@ public class GLaDOS {
             System.out.print("> ");
             input = scanner.nextLine();
             
+            GLaDOS.printLine();
+
             // If input matches exit command, stop listening
             if (input.equals(exitCommand)) {
                 break;
             }
             
-            // Else echo the input
-            System.out.println("___________________________________________________________________________________");
-            System.out.println(input);
-            System.out.println("___________________________________________________________________________________\n");
+            // If input matches list command, print list of stored text
+            if (input.equals(listCommand)) {
+                // Print stored list
+                for (int i = 0; i < storedList.size(); i++) {
+                    System.out.println((i + 1) + ". " + storedList.get(i));
+                }
+            } else {
+                // Else add input to list
+                storedList.add(input);
+                System.out.println("added: " + input);
+            }
+            
+            GLaDOS.printLine();
         }
         scanner.close();
     }
 
     public static void outro() {
-        System.out.println("___________________________________________________________________________________");
         System.out.println("Goodbye. Thank you for participating in this Aperture Science test.");
-        System.out.println("___________________________________________________________________________________");
+        GLaDOS.printLine();
+    }
+
+    public static void printLine() {
+        String lineChar = "\u2500";
+        for (int i = 0; i < 83; i++) {
+            System.out.print(lineChar);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
