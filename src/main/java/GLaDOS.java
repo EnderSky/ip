0a9a7740981014da2851c3cedce1a43c654228f4
EnderSky <jothamlimjl@hotmail.com>
2026-01-23 14:02:02 +0800

    feat: renamed bot, added greet and exit text

---
+++ src/main/java/GLaDOS.java
@@ -0,0 +1,22 @@
+public class GLaDOS {
+    public static void main(String[] args) {
+        String logo =   "                                                                          \r\n" + //
+                        "                                                                          \r\n" + //
+                        "   █████████  █████                 ██████████      ███████     █████████ \r\n" + //
+                        "  ███▒▒▒▒▒███▒▒███                 ▒▒███▒▒▒▒███   ███▒▒▒▒▒███  ███▒▒▒▒▒███\r\n" + //
+                        " ███     ▒▒▒  ▒███         ██████   ▒███   ▒▒███ ███     ▒▒███▒███    ▒▒▒ \r\n" + //
+                        "▒███          ▒███        ▒▒▒▒▒███  ▒███    ▒███▒███      ▒███▒▒█████████ \r\n" + //
+                        "▒███    █████ ▒███         ███████  ▒███    ▒███▒███      ▒███ ▒▒▒▒▒▒▒▒███\r\n" + //
+                        "▒▒███  ▒▒███  ▒███      █ ███▒▒███  ▒███    ███ ▒▒███     ███  ███    ▒███\r\n" + //
+                        " ▒▒█████████  ███████████▒▒████████ ██████████   ▒▒▒███████▒  ▒▒█████████ \r\n" + //
+                        "  ▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒▒▒▒  ▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒▒▒▒▒      ▒▒▒▒▒▒▒     ▒▒▒▒▒▒▒▒▒  \r\n" + //
+                        "                                                                          \r\n" + //
+                        "                                                                          ";
+        System.out.println(logo);
+        System.out.println("Hello, and again, welcome to the Aperture Science Computer-Aided Enrichment Center.");
+        System.out.println("What can I do for you today?");
+        System.out.println("___________________________________________________________________________________");
+        System.out.println("Goodbye. Thank you for participating in this Aperture Science test.");
+        System.out.println("___________________________________________________________________________________");
+    }
+}
