1eb99cbeb7cb1ce746ab51f0825b999e06687954
Jotham Lim <EnderSky@users.noreply.github.com>
2026-02-19 00:54:47 +0800

    Reapply "Merge branch 'branch-A-Assertions'"
    This reverts commit 14e9da215965c1ce3e42d1e2411a95d8c0ba6f5b.

--- src/main/java/glados/task/Deadline.java
+++ src/main/java/glados/task/Deadline.java
@@ -18,6 +18,7 @@ public class Deadline extends Task {
      */
     public Deadline(String description, LocalDateTime by) {
         super(description);
+        assert by != null : "Deadline time should not be null";
         this.tag = 'D';
         this.by = by;
     }
